989f0d2e1eaecffea4bfd7064524c1934f52f7b1
kristinadarroch <dar15003@byui.edu>
2018-10-08 19:42:44 -0600

    ch3 partially

--- cms/src/app/messages/message-edit/message-edit.component.ts
+++ cms/src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
+  public currentSender: string = 'Kristina Darroch';
 
+   @ViewChild('subject') subjectRef: ElementRef;
+   @ViewChild('msgText') msgTextRef: ElementRef;
+
+   @Output() addMessageEvent = new EventEmitter<Message>();
+  
+  onSendMessage(){
+    console.log('the subject');
+    const subject = this.subjectRef.nativeElement.value;
+    const msgText = this.msgTextRef.nativeElement.value;
+    const message = new Message(1, subject, msgText, this.currentSender);
+    this.addMessageEvent.emit(message);
+  }
+
+  onClear(){
+    console.log('cleared');
+    this.subjectRef.nativeElement.value = '';
+    this.msgTextRef.nativeElement.value = '';
+  }
   constructor() { }
 
   ngOnInit() {
   }
-
 }
--- cms/src/app/messages/message-list/message-list.component.ts
+++ cms/src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent implements OnInit {
+  message: Message[] = [
+    new Message(1, 'Homework', 'Hey can you help me with my homework tonight?', 'Nathan Thorne'),
+    new Message(2, 'Missing Class Today', 'Hey I will not be able to make it to class I am sick', 'Brother Thayne'),
+    new Message(3, 'Extra Credit', 'Hey my grade sucks can you help me out?', 'Kent Jackson')
+  ]
+  onAddMessage(message: Message){
+    this.message.push(message);
+  }
 
   constructor() { }
 
