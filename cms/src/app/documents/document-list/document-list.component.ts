bcc25655a508cc5d975cc97d07f725e429ae1cf9
kristinadarroch <dar15003@byui.edu>
2018-10-11 22:59:42 -0600

    directives

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
+  documents: Document[] = [
+    new Document(1, 'Drake', 'The rapper', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Drake_at_the_Velvet_Underground_-_2017_%2835986086223%29_%28cropped%29.jpg/220px-Drake_at_the_Velvet_Underground_-_2017_%2835986086223%29_%28cropped%29.jpg'),
+    new Document(2, 'Quavo', 'Member of migos','https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/10/quavo-migos-GettyImages-1037749308.jpg'),
+    new Document(3, 'Offset', 'Another member of migos', 'https://en.wikipedia.org/wiki/Offset_(rapper)'),
+    new Document(4, 'TakeOff','The last member of migos' ,'https://en.wikipedia.org/wiki/Takeoff_(rapper)')
+  ]
   constructor() { }
 
   ngOnInit() {
+    console.log(this.documents);
+  }
+
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
+    console.log(this.documents);
+    
   }
 
 }
